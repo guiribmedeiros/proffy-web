be4fad31f3c208e711b1d65cb3882fa41f7b7f9c
Guilherme Medeiros <guiribmedeiros@gmail.com>
2020-09-02 01:44:35 -0300

    feat(config): set the default port value
    Sets the default port value to empty instead of undefined.

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,5 @@
 export default {
     api_protocol: process.env.REACT_APP_API_PROTOCOL || 'http',
     api_host: process.env.REACT_APP_API_HOST || 'localhost',
-    api_port: process.env.REACT_APP_API_PORT,
+    api_port: process.env.REACT_APP_API_PORT || '',
 }
